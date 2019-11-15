a775c5918275aced35bb4aaa662fa233fd185f7e
pe <pe@pe.com>
2019-11-15 16:57:58 +0100

    componenti funzionali, di classi e stato

--- react_prima_lezione/first-app/src/App.js
+++ react_prima_lezione/first-app/src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import HeaderZaurdo from './HeaderZaurdo';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    // call constructor of mother class
+    super(props)
+
+    this.state = {
+      nome: "saro",
+      n: 0
+
+    }
+  }
+
+  updateN = () => {
+
+    setInterval(() => {
+      this.setState({
+        n: this.state.n += 1
+      })
+
+    }, 1000);
+
+  }
+
+  // viene invocato successivamente al render
+  componentDidMount() {
+    // this.updateN()
+  }
+  // viene invocato non appena il componente viene renderizzato
+  render() {
+
+    return (
+      // sintassi per creare un wrapper senza specificare header
+      <>
+        <HeaderZaurdo />
+        <div className="App">
+        </div>
+      </>
+    )
+  }
 }
 
 export default App;
---
+++ react_prima_lezione/first-app/src/HeaderZaurdo.js
@@ -0,0 +1,33 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './HeaderZaurdo.css';
+// import './App';
+
+class HeaderZaurdo extends React.Component {
+  
+
+
+  
+  componentDidMount(){
+  }
+  render() {
+    return (
+      <div className="HeaderZaurdo">
+        <header>
+          <img src="/logo512.png" className="logo" ></img>
+        </header>
+        <nav>
+          <ul>
+            <li>ciao </li>
+            <li>mbare</li>
+            <li>ciao</li>
+          </ul>
+        </nav>
+      </div>
+    )
+  }
+}
+
+
+
+export default HeaderZaurdo;
